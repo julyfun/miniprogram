4c1ec8c35ad3f74b13b6cb69c3d7bf8a3b2cf289
julyfun-win11-4070s <julyfun.collect@outlook.com>
2025-04-28 10:42:26 +0800

    fix: 拓展视频列表

--- miniprogram/pages/food-guide/food-guide.ts
+++ miniprogram/pages/food-guide/food-guide.ts
@@ -9,7 +9,12 @@ const VIDEO_OPTIONS = [
     '002.mp4',
     '003.mp4',
     '004.mp4',
-    '005.mp4'
+    '005.mp4',
+    '006.mp4',
+    '007.mp4',
+    '008.mp4',
+    '009.mp4',
+    '010.mp4',
 ];
 
 // Get a random video URL from the available options
